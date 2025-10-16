a3606c4e988f296271f84f34480793f9dd6b3d2f
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-16 08:55:30 +0530

    created the cricbuzz-backend

---
+++ src/main/java/com/company/cricbuzz_backend/CricbuzzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.cricbuzz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricbuzzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricbuzzBackendApplication.class, args);
+	}
+
+}
