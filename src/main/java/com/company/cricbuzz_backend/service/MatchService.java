5a312b60cf6ad366b1900a6cf9a7d9fb98b74d69
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-10-16 11:03:36 +0530

    created the match service interface and defined methods

---
+++ src/main/java/com/company/cricbuzz_backend/service/MatchService.java
@@ -0,0 +1,30 @@
+package com.company.cricbuzz_backend.service;
+
+import com.company.cricbuzz_backend.dtos.CommentaryEventDto;
+import com.company.cricbuzz_backend.dtos.MatchDto;
+import com.company.cricbuzz_backend.dtos.ScoreSnapshotDto;
+
+import java.util.List;
+
+public interface MatchService {
+
+
+    //========= Match related =============//
+    MatchDto getMatchById(Long matchId);
+    List<MatchDto> getAllMatches();
+    List<MatchDto> getLiveMatches();
+    List<MatchDto> getMatchesByTeam(String teamName);
+
+
+    //=========score related =============//
+    ScoreSnapshotDto getLatestScore(Long matchId);
+    List<ScoreSnapshotDto> getScoreHistory(Long matchId);
+
+
+    //========= commentary related ========//
+    List<CommentaryEventDto> getCommentaryByMatch(Long matchId);
+
+    // updated
+    void updateMatchScore(ScoreSnapshotDto scoreSnapshotDto);
+    void addCommentaryEvent(CommentaryEventDto commentaryEventDto);
+}
