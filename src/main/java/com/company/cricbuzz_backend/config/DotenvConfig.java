734ffb0f3417615c4b4b463e74f86ad54bce1bac
98001yash <yashchauhan.gaya@gmail.com>
2026-08-30 22:10:07 +0530

    added the dotenvConfig

---
+++ src/main/java/com/company/cricbuzz_backend/config/DotenvConfig.java
@@ -0,0 +1,20 @@
+package com.company.cricbuzz_backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+
+    static {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+    }
+}
